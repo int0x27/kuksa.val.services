2678a65918b2e9156c582857acac3687a0949d4d
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2022-07-12 18:57:41 +0300

    Hackathon2 guineapigs++ (#18)
    trunk service improvements
    - added periodic location feeding with random values
    - added color logging
    
    testclient also improved:
    - added --inst argument for changing default trunk instance(ALL, FRONT, REAR)
    
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- trunk_service/testclient.py
+++ trunk_service/testclient.py
@@ -40,70 +40,65 @@ class TrunkTestClient(object):
         # bind the client and the server
         self.stub = pb2_grpc.TrunkStub(self.channel)
 
-    def execute_methods(self, lock_state: LockState, open_state: bool) -> None:
+    def execute_methods(self, lock_state: LockState, open_state: bool, inst: TrunkInstance=TrunkInstance.REAR) -> None:
         """
         Client function to call the rpc for TrunkService methods
         """
-        logger.info("Setting Lock State: %s", self.get_trunk_str(lock_state))
-        request = pb2.SetLockStateRequest(instance=TrunkInstance.REAR, state=lock_state)
+        logger.info("Setting %s Trunk Lock State: %s", inst, lock_state)
+        request = pb2.SetLockStateRequest(instance=inst, state=lock_state)
         self.stub.SetLockState(request)
 
         if open_state:
-            logger.info("Opening Trunk (if not already) ...")
-            request = pb2.OpenRequest(instance=TrunkInstance.REAR)
+            logger.info("Opening %s Trunk (if not already) ...", str(inst))
+            request = pb2.OpenRequest(instance=inst)
             self.stub.Open(request)
         else:
-            logger.info("Closing Trunk (if not already) ...")
-            request = pb2.CloseRequest(instance=TrunkInstance.REAR)
+            logger.info("Closing %s Trunk (if not already) ...", str(inst))
+            request = pb2.CloseRequest(instance=inst)
             self.stub.Close(request)
 
         logger.info("Done.")
-
-    def get_trunk_str(self, trunk_value) -> str:
-        if trunk_value == 0:
-            return "LockState.UNLOCKED"
-        elif trunk_value == 1:
-            return "LockState.LOCKED"
-        else:
-            return "Invalid value: {}".format(trunk_value)
 
 
 def main(argv):
     """Main function"""
 
     default_addr = "127.0.0.1:50053"
-    default_lockstate = "1"
+    default_instance = "REAR"
+    default_lockstate = "LOCKED"
     default_openstate = "0"
 
     _usage = (
         "Usage: ./testclient.py --addr <host:name>"  # shorten line
-        " --lock=LOCK_STATE --open=OPEN_STATE\n\n"
+        "--inst=INSTANCE --lock=LOCK_STATE --open=OPEN_STATE\n\n"
         "Environment:\n"
         "  'VDB_ADDR'    Databroker address (host:port). Default: {}\n"
-        "  'LOCK_STATE'  Trunk Lock State (0=unlocked, 1=locked). Default: {}\n"
+        "  'INSTANCE'    Trunk Instance   (ALL=0, FRONT=1, REAR=2). Default: {}\n"
+        "  'LOCK_STATE'  Trunk Lock State (0=UNLOCKED, 1=LOCKED). Default: {}\n"
         "  'OPEN_STATE'  Trunk Open State (0=closed, 1=open). Default: {}\n".format(
-            default_addr, default_lockstate, default_openstate
+            default_addr, default_instance, default_lockstate, default_openstate
         )
     )
 
     # environment values (overridden by cmdargs)
     trunk_addr = os.getenv("TRUNK_ADDR", default_addr)
-    lock_state = (
-        LockState.LOCKED if os.getenv("LOCK_STATE") != "0" else LockState.UNLOCKED
-    )
+    instance = TrunkInstance.Value(os.getenv("INSTANCE", default_instance))
+    lock_state = LockState.Value(os.getenv("LOCK_STATE", default_lockstate))
     open_state = True if os.getenv("OPEN_STATE") != "0" else False
 
     # parse cmdline args
     try:
-        opts, args = getopt.getopt(argv, "ha:l:o:", ["addr=", "lock=", "open="])
+        opts, args = getopt.getopt(argv, "ha:i:l:o:", ["addr=", "inst=", "lock=", "open="])
         for opt, arg in opts:
             if opt == "-h":
                 print(_usage)
                 sys.exit(0)
             elif opt in ("-a", "--addr"):
                 trunk_addr = arg
+            elif opt in ("-i", "--inst"):
+                instance = TrunkInstance.Name(int(arg)) if arg.isnumeric() else TrunkInstance.Value(arg)
             elif opt in ("-l", "--lock"):
-                lock_state = LockState.LOCKED if arg != "0" else LockState.UNLOCKED
+                lock_state = LockState.Name(int(arg)) if arg.isnumeric() else LockState.Value(arg)
             elif opt in ("-o", "--open"):
                 open_state = True if arg != "0" else False
             else:
@@ -115,7 +110,7 @@ def main(argv):
         sys.exit(1)
 
     client = TrunkTestClient(trunk_addr)
-    client.execute_methods(lock_state, open_state)
+    client.execute_methods(lock_state, open_state, instance)
 
 
 if __name__ == "__main__":
